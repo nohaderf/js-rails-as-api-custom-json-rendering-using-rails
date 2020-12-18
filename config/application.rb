9192c60b8d647c3a06dfa67c4cb7619ce8f755e5
nohaderf <freda.hon@gmail.com>
2020-12-18 13:48:14 -0500

    Done.

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,16 @@ module BirdWatcher
       end
     end
 
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*',
+          :headers => :any,
+          :methods => [:get, :post, :delete, :put, :patch, :options, :head],
+          :max_age => 0
+      end
+    end 
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
